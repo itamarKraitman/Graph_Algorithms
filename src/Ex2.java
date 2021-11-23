5a2beb7d42463626154d86a60e20137156f91e7a
itamarKraitman <73484628+itamarKraitman@users.noreply.github.com>
2021-11-23 19:40:48 +0200

    first commit

---
+++ src/Ex2.java
@@ -0,0 +1,46 @@
+import api.DirectedWeightedGraph;
+import api.DirectedWeightedGraphAlgorithms;
+
+/**
+ * This class is the main class for Ex2 - your implementation will be tested using this class.
+ * TODO: implement the data structure - hashMap: key- vertex id, values- hash map: key- destination, values- weight.
+ * TODO: implement all classes according to data structure attitude.
+ * TODO:
+ */
+public class Ex2 {
+    /**
+     * This static function will be used to test your implementation
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraph getGrapg(String json_file) {
+        DirectedWeightedGraph ans = null;
+        // ****** Add your code here ******
+        //
+        // ********************************
+        return ans;
+    }
+    /**
+     * This static function will be used to test your implementation
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraphAlgorithms getGrapgAlgo(String json_file) {
+        DirectedWeightedGraphAlgorithms ans = null;
+        // ****** Add your code here ******
+        //
+        // ********************************
+        return ans;
+    }
+    /**
+     * This static function will run your GUI using the json fime.
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     *
+     */
+    public static void runGUI(String json_file) {
+        DirectedWeightedGraphAlgorithms alg = getGrapgAlgo(json_file);
+        // ****** Add your code here ******
+        //
+        // ********************************
+    }
+}
