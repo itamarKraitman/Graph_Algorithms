113f7763b5bc8411d1f8ec150449afe001156104
itamarKraitman <itamar.kraitman@msmail.ariel.ac.il>
2021-12-08 14:09:10 +0200

    panel

---
+++ src/main/java/GUI/Panel.java
@@ -0,0 +1,13 @@
+package main.java.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel extends JPanel {
+
+    public Panel() {
+        this.setPreferredSize(new Dimension(500, 500));
+        this.setBackground(Color.WHITE);
+    }
+
+}
